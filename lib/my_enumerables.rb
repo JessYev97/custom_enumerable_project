5d1d1f9d108d1c2ec62903db356cf644879ed834
JessYev97 <jessmyeverino@gmail.com>
2025-04-10 14:13:56 -0500

    add logic for each and each_with_index

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -8,4 +8,20 @@ end
 # to this method
 class Array
   # Define my_each here
+  def my_each
+    return enum_for(:my_each) unless block_given?
+    each do |element| 
+      yield(element) 
+    end 
+    self 
+  end 
+
+  def my_each_with_index
+    index = -1 
+    each do |element| 
+      index += 1 
+      yield(element, index) 
+      self 
+    end 
+  end
 end
